ff1ffd829f997d63d94ef648ff0ca8cf86c24286
Yodle_94 <sj_artist@naver.com>
2023-11-29 15:29:16 +0900

    [Feat] 종료 조건 처리

--- Othello/OthelloForm.Designer.cs
+++ Othello/OthelloForm.Designer.cs
@@ -30,16 +30,41 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OthelloForm));
             groupBox1 = new GroupBox();
+            lb_blackScore = new Label();
+            lb_whiteScore = new Label();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(lb_whiteScore);
+            groupBox1.Controls.Add(lb_blackScore);
             groupBox1.Location = new Point(264, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(60, 245);
+            groupBox1.Size = new Size(60, 72);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "Info";
+            // 
+            // lb_blackScore
+            // 
+            lb_blackScore.AutoSize = true;
+            lb_blackScore.BackColor = SystemColors.ControlDarkDark;
+            lb_blackScore.ForeColor = SystemColors.Control;
+            lb_blackScore.Location = new Point(6, 19);
+            lb_blackScore.Name = "lb_blackScore";
+            lb_blackScore.Size = new Size(39, 15);
+            lb_blackScore.TabIndex = 0;
+            lb_blackScore.Text = "label1";
+            // 
+            // lb_whiteScore
+            // 
+            lb_whiteScore.AutoSize = true;
+            lb_whiteScore.BackColor = SystemColors.ControlLightLight;
+            lb_whiteScore.Location = new Point(6, 44);
+            lb_whiteScore.Name = "lb_whiteScore";
+            lb_whiteScore.Size = new Size(39, 15);
+            lb_whiteScore.TabIndex = 1;
+            lb_whiteScore.Text = "label2";
             // 
             // OthelloForm
             // 
@@ -50,11 +75,15 @@
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "OthelloForm";
             Text = "Othello";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private GroupBox groupBox1;
+        private Label lb_whiteScore;
+        private Label lb_blackScore;
     }
 }
--- Othello/OthelloForm.cs
+++ Othello/OthelloForm.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace Othello
 {
     public partial class OthelloForm : Form
@@ -7,6 +9,9 @@ namespace Othello
         private bool _side;
         private readonly int[] dy = { +1, +1, +0, -1, -1, -1, +0, +1 };
         private readonly int[] dx = { +0, +1, +1, +1, +0, -1, -1, -1 };
+        private bool _gameover;
+        private int _whiteCount;
+        private int _blackCount;
 
 
         public OthelloForm()
@@ -38,7 +43,7 @@ namespace Othello
 
                     if ((i == 3 && j == 3) || (i == 4 && j == 4))
                     {
-                        if(button.Tag is StoneInfo info)
+                        if (button.Tag is StoneInfo info)
                             info.Side = false;
                         button.BackColor = Color.White;
                     }
@@ -54,10 +59,17 @@ namespace Othello
                     Controls.Add(button);
                 }
             }
+
+            _blackCount = _whiteCount = 2;
+            lb_blackScore.Text = _blackCount.ToString();
+            lb_whiteScore.Text = _whiteCount.ToString();
         }
 
         private void BoardButtonClick(object sender, EventArgs e)
         {
+            if (_gameover)
+                return;
+
             var button = (Button)sender;
 
             if (button.Tag is StoneInfo stoneInfo)
@@ -83,7 +95,7 @@ namespace Othello
                             if (nextStoneInfo.Side == null)
                                 break;
 
-                            if(nextStoneInfo.Side == _side)
+                            if (nextStoneInfo.Side == _side)
                             {
                                 nearestPointByDirection[dir] = nextPoint;
                                 break;
@@ -95,21 +107,22 @@ namespace Othello
                 }
 
                 bool isValid = false;
-                for(int dir = 0; dir < 8; dir ++)
+                int count = 0;
+                for (int dir = 0; dir < 8; dir++)
                 {
                     if (!nearestPointByDirection.TryGetValue(dir, out var startPoint) || startPoint == null)
                         continue;
 
                     bool check = false;
                     List<Button> checkBtns = new List<Button>();
-                    for(int dist = _boardSize - 1; dist >= 1; dist--)
+                    for (int dist = _boardSize - 1; dist >= 1; dist--)
                     {
                         var nextPoint = Point.Add(pivot, new Size(dist * dx[dir], dist * dy[dir]));
 
                         if (nextPoint.X < 0 || nextPoint.Y < 0 || nextPoint.X > _boardSize - 1 || nextPoint.Y > _boardSize - 1)
                             continue;
 
-                        if(startPoint.Value.X == nextPoint.X && startPoint.Value.Y == nextPoint.Y)
+                        if (startPoint.Value.X == nextPoint.X && startPoint.Value.Y == nextPoint.Y)
                         {
                             check = true;
                             continue;
@@ -126,14 +139,62 @@ namespace Othello
                             nextStoneInfo.Side = _side;
                         checkBtn.BackColor = _side ? Color.Black : Color.White;
                     }
+
+                    count += checkBtns.Count;
                 }
 
-                if(isValid)
+                if (isValid)
                 {
                     stoneInfo.Side = _side;
                     button.BackColor = _side ? Color.Black : Color.White;
+
+                    if (_side)
+                    {
+                        _blackCount = 1 + _blackCount + count;
+                        _whiteCount -= count;
+                    }
+                    else
+                    {
+                        _whiteCount = 1 + _whiteCount + count;
+                        _blackCount -= count;
+                    }
+
+                    lb_blackScore.Text = _blackCount.ToString();
+                    lb_whiteScore.Text = _whiteCount.ToString();
+
                     _side = !_side;
+
+                    if (_blackCount == 0 || _whiteCount == 0 || _whiteCount + _blackCount == _boardSize * _boardSize)
+                    {
+                        _gameover = true;
+                        ShowGameOverMessageBox();
+                    }
                 }
+            }
+        }
+
+        private void ShowGameOverMessageBox()
+        {
+            string target;
+            if (_blackCount > _whiteCount)
+                target = "BLACK";
+            else if (_blackCount < _whiteCount)
+                target = "WHITE";
+            else
+                target = "both";
+
+            var message = $"Game Over! {target} wins!";
+            var result = MessageBox.Show(message, "Game Over", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                _gameover = false;
+                InitializeComponent();
+                InitializeBoard();
+            }
+            else if (result == DialogResult.No)
+            {
+                Application.Exit();
             }
         }
     }
