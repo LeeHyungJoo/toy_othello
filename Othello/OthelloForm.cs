d89f337bb8e8f1ea746a2a9073e1ee13a59d1929
Yodle_94 <sj_artist@naver.com>
2023-11-29 14:23:27 +0900

    [Feat] 기본적인 로직은 구현, 다만 색칠하는 로직이 보강 필요함.

--- Othello/OthelloForm.cs
+++ Othello/OthelloForm.cs
@@ -5,6 +5,9 @@ namespace Othello
         private const int _boardSize = 8;
         private Button[,] _btns;
         private bool _side;
+        private readonly int[] dy = { +1, +1, +0, -1, -1, -1, +0, +1 };
+        private readonly int[] dx = { +0, +1, +1, +1, +0, -1, -1, -1 };
+
 
         public OthelloForm()
         {
@@ -32,6 +35,21 @@ namespace Othello
                     };
 
                     button.Click += BoardButtonClick!;
+
+                    if ((i == 3 && j == 3) || (i == 4 && j == 4))
+                    {
+                        if(button.Tag is StoneInfo info)
+                            info.Side = false;
+                        button.BackColor = Color.White;
+                    }
+
+                    if ((i == 3 && j == 4) || (i == 4 && j == 3))
+                    {
+                        if (button.Tag is StoneInfo info)
+                            info.Side = true;
+                        button.BackColor = Color.Black;
+                    }
+
                     _btns[i, j] = button;
                     Controls.Add(button);
                 }
@@ -42,16 +60,51 @@ namespace Othello
         {
             var button = (Button)sender;
 
-            if(button.Tag is  StoneInfo stoneInfo)
+            if (button.Tag is StoneInfo stoneInfo)
             {
-                if(stoneInfo.Side != null)
-                {
+                if (stoneInfo.Side != null)
                     return;
+
+                //8방향 모두 체크
+                bool isValid = false;
+                var pivot = stoneInfo.Coord;
+                for(int dir = 0; dir < 8; dir ++)
+                {
+                    bool check = false;
+                    for(int dist = _boardSize - 1; dist >= 0; dist--)
+                    {
+                        var nextPoint = Point.Add(pivot, new Size(dist * dx[dir], dist * dy[dir]));
+
+                        if (nextPoint.X < 0 || nextPoint.Y < 0 || nextPoint.X > _boardSize - 1 || nextPoint.Y > _boardSize - 1)
+                            continue;
+
+                        if (_btns[nextPoint.X,nextPoint.Y].Tag is StoneInfo nextStoneInfo)
+                        {
+                            if(nextStoneInfo.Side == null)
+                                continue;
+
+                            if(nextStoneInfo.Side == _side)
+                            {
+                                check = true;
+                                continue;
+                            }
+
+                            if(check)
+                            {
+                                isValid = true;
+                                nextStoneInfo.Side = _side;
+                                _btns[nextPoint.X, nextPoint.Y].BackColor = _side ? Color.Black : Color.White;
+                            }
+                        }
+                    }
                 }
 
-                stoneInfo.Side = _side;
-                button.BackColor = _side ? Color.Black : Color.White;
-                _side = !_side;
+                if(isValid)
+                {
+                    stoneInfo.Side = _side;
+                    button.BackColor = _side ? Color.Black : Color.White;
+                    _side = !_side;
+                }
             }
         }
     }
